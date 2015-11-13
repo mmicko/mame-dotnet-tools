731d21b75846fd59625cdd2a4f0b898845c0b92d
Miodrag Milanovic <mmicko@gmail.com>
2015-11-13 13:39:53 +0100

    Added few more tools (nw)

---
+++ CheckIncludedFiles/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIncludedFiles
+{
+    class Program
+    {
+        static List<string> files;
+        static List<string> srcfiles;
+        static string root = @"c:\buildtools\src\mame\";
+
+        static void ParseFile(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            foreach (string line in lines)
+            {
+                // regular files
+                if (line.Trim().StartsWith("MAME_DIR .. \""))
+                {
+                    string l = line.Trim().Replace("MAME_DIR .. \"", "").Replace("\",", "").Replace("\"","");
+                    files.Add(l);
+                }
+                // commented like some test disassms and similar
+                if (line.Trim().StartsWith("--MAME_DIR .. \""))
+                {
+                    string l = line.Trim().Replace("--MAME_DIR .. \"", "").Replace("\",", "").Replace("\"", "");
+                    files.Add(l);
+                }
+                // disassebler extraction
+                if (line.Trim().StartsWith("table.insert(disasm_files , "))
+                {
+                    string l = line.Trim().Replace("table.insert(disasm_files , ","").Replace("MAME_DIR .. \"", "").Replace("\")", "");
+                    files.Add(l);
+                }
+            }
+        }
+
+        public static void ScanSource(string path)
+        {
+            srcfiles = new List<string>();
+
+            if (File.Exists(path))
+            {
+                // This path is a file
+                ProcessFile(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                // This path is a directory
+                ProcessDirectory(path);
+            }
+        }
+
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        public static void ProcessFile(string path)
+        {
+            string l = path.Replace(root, "").Replace(@"\",@"/");
+            if (!files.Contains(l))
+                srcfiles.Add(l);
+        }
+
+        static void Main(string[] args)
+        {
+            files = new List<string>();
+
+            ParseFile(root + @"scripts\src\bus.lua");
+            ParseFile(root + @"scripts\src\cpu.lua");
+            ParseFile(root + @"scripts\src\devices.lua");
+            ParseFile(root + @"scripts\src\emu.lua");
+            ParseFile(root + @"scripts\src\lib.lua");
+            ParseFile(root + @"scripts\src\machine.lua");
+            ParseFile(root + @"scripts\src\main.lua");
+            ParseFile(root + @"scripts\src\netlist.lua");
+            ParseFile(root + @"scripts\src\sound.lua");
+            ParseFile(root + @"scripts\src\tools.lua");
+            ParseFile(root + @"scripts\src\video.lua");
+
+            ParseFile(root + @"scripts\src\osd\modules.lua");
+            ParseFile(root + @"scripts\src\osd\osdmini.lua");
+            ParseFile(root + @"scripts\src\osd\sdl.lua");
+            ParseFile(root + @"scripts\src\osd\windows.lua");
+
+            ParseFile(root + @"scripts\target\ldplayer\ldplayer.lua");
+ 
+            ParseFile(root + @"scripts\target\mame\arcade.lua");
+            ParseFile(root + @"scripts\target\mame\dummy.lua");
+            ParseFile(root + @"scripts\target\mame\mame.lua");
+            ParseFile(root + @"scripts\target\mame\mess.lua");
+            ParseFile(root + @"scripts\target\mame\nl.lua");
+            ParseFile(root + @"scripts\target\mame\tiny.lua");
+
+            ScanSource(root + @"src\");
+
+            foreach (string file in srcfiles) 
+            {
+                if (file.EndsWith(".h") || file.EndsWith(".cpp") || file.EndsWith(".inc"))
+                    Console.WriteLine(file);
+            }
+            
+        }
+    }
+}
---
+++ LogErrorChange/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogErrorChange
+{
+    class Program
+    {
+
+        public static string[] changefile(string fileContent, string filename)
+        {
+            fileContent = fileContent.Substring(0, fileContent.Length - 2);
+            string[] lines = fileContent.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Replace("\n", "").Replace("\r", "");
+            }
+
+            bool changed = false;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("logerror(") && !lines[i].Contains("logerror(LOG_"))
+                {
+                    lines[i] = lines[i].Replace("logerror(", "logerror(LOG_DEFAULT,");
+                    changed = true;
+                }
+               
+
+            }
+            if (changed)
+            {
+                System.IO.File.WriteAllLines(filename, lines);
+            }
+            return lines;
+        }
+
+        static void Analize(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path, "*.cpp");
+            
+            foreach (string filename in filePaths)
+            {
+                string fileContent;
+                if (File.Exists(filename))
+                {
+                    using (StreamReader sr = new StreamReader(filename))
+                    {
+                        fileContent = sr.ReadToEnd();
+                    }
+                    string fn = Path.GetFileName(filename);
+                    //Console.WriteLine(fn);
+                    //Console.WriteLine("-----------");
+
+                    string[] lines = changefile(fileContent, filename);
+                }
+
+            }
+        }
+        static void AnalizeDir(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path);          
+            foreach (string dir in dirs)
+            {
+                AnalizeDir(dir);
+                Analize(dir);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string root = @"c:\buildtools\src\mame\";
+            AnalizeDir(root + @"\src\devices\");
+        }
+    }
+}
